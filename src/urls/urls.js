c5443b17167c7d441d9a939eccde447a8638c0c2
Krishnanunni.S <krishnanunni2424@gmail.com>
2023-01-03 01:30:04 +0530

    first commit

---
+++ src/urls/urls.js
@@ -0,0 +1,11 @@
+import { API_KEY } from "../constants/constants";
+export const originals =  `discover/tv?api_key=${API_KEY}&with_networks=213`;
+export const action=`discover/movie?api_key=${API_KEY}&with_genres=28`
+export const comedy=`discover/movie?api_key=${API_KEY}&with_genres=35`
+export const thriller=`discover/movie?api_key=${API_KEY}&with_genres=53`
+export const scienceFiction=`discover/movie?api_key=${API_KEY}&with_genres=878`
+
+
+
+
+
